20376c5e540c00c26121470d12e6749a93ad461c
u1712 <extreme_wrongdoer@proton.me>
2025-09-20 05:53:08 +0100

    first working version

--- main.c
+++ main.c
@@ -8,33 +8,33 @@
 int main(void) {
 	srand(time(NULL));
 	
-	printf("Welcome to the random number guessing game\n\tEnjoy!");
+	printf("Welcome to the random number guessing game\n\tEnjoy!\n");
 
-	char *max_str = readline("What's the maximum value:\n");
+	char *max_str = readline("What's the maximum value?\n");
 	int max = atoi(max_str);
-	char *min_str = readline("What's the minimum value:\n");
+	char *min_str = readline("What's the minimum value?\n");
 	int min = atoi(min_str);
 
-	int chances = 100 / (max - min + 1);
+	double chances = 100.0 / (max - min + 1);
 
 	if (max <= min) {
-		printf("THE MINIMUM VALUE CAN'T BE MORE THAN THE MAXIMUM\nget out\n");
+		printf("THE MINIMUM VALUE CAN'T BE MORE THAN(THE SAME AS) THE MAXIMUM\nget out\n");
 		exit(1);
 	}
 	
-	printf("Are you really sure?\nYour chances of winning are : %d%%\n!!", chances);
+	printf("Are you really sure?\nYour chances of winning are : %.2f%%!!\n", chances);
 	char *answer = readline("(y/n): \n");
-	if (strcmp(answer, "y") == 0) {
+	if ((strcmp(answer, "y") == 0) || (strcmp(answer, "Y") == 0)) {
 		int r = rand() % (max - min + 1) + min;
 		char *usr_str = readline("Enter your guess: \n");
 		int usr = atoi(usr_str);
 		if (r == usr) {
-			printf("you cheated right -_-\nnot cool");
+			printf("you cheated right -_-\nnot cool\n");
 		} else {
-			printf("You WON ....... jk you lost what did you expect\n");
+			printf("You WON ....... jk you lost what did you expect\nit was: %d\n", r);
 		}
 		free(usr_str);
-	} else if (strcmp(answer, "n") == 0) {
+	} else if ((strcmp(answer, "n") == 0) || (strcmp(answer, "N") == 0)) {
 		printf("coward\n");	
 	}
 	free(max_str);
